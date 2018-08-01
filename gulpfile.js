f281428572af0735a8616a9354861a479dd97aae
Andrey Margashov <Andrey.Margashov@yandex.ru>
2018-08-01 10:32:57 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var cleanCss = require('gulp-clean-css');
+var plumber = require('gulp-plumber');
+var gulpIf = require('gulp-if');
+var browserSync = require('browser-sync').create();
+
+var config = {
+    paths: {
+        less: './src/less/**/*.less',
+        html: './public/index.html'
+    },
+    output: {
+        cssName: 'bundle.min.css',
+        path: './public'
+    },
+    isDevelop: true
+};
+
+gulp.task('less', function () {
+    return gulp.src(config.paths.less)
+        .pipe(plumber())
+        .pipe(gulpIf(config.isDevelop,sourcemaps.init()))
+        .pipe(less())
+        .pipe(concat(config.output.cssName))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulpIf(!config.isDevelop, cleanCss()))
+        .pipe(gulpIf(config.isDevelop, sourcemaps.write()))
+        .pipe(gulp.dest(config.output.path))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: config.output.path
+        }
+    });
+    gulp.watch(config.paths.less, ['less']);
+    gulp.watch(config.paths.html).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['less','serve']);
